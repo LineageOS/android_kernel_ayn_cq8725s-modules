7c567b66e4a004d1ff0d58db75269eed5c640849
Balázs Triszka <info@balika011.hu>
2026-05-14 00:21:46 +0200

    audio: Add aw883xx codec
    Change-Id: Ieef64b903efa2626ef9c4361d723bde2e8c1743b

--- qcom/opensource/audio-kernel/audio_kernel_modules.mk
+++ qcom/opensource/audio-kernel/audio_kernel_modules.mk
@@ -51,7 +51,8 @@ AUDIO_KERNEL_MODULES += $(KERNEL_MODULES_OUT)/swr_dmic_dlkm.ko \
 endif
 ifeq ($(call is-board-platform-in-list,sun tuna), true)
 AUDIO_KERNEL_MODULES += $(KERNEL_MODULES_OUT)/lpass_bt_swr_dlkm.ko
-AUDIO_KERNEL_MODULES += $(KERNEL_MODULES_OUT)/qmp_dlkm.ko
+	$(KERNEL_MODULES_OUT)/qmp_dlkm.ko \
+	$(KERNEL_MODULES_OUT)/aw883xx_dlkm.ko
 endif
 ifeq ($(call is-board-platform-in-list,bengal holi blair), true)
 AUDIO_KERNEL_MODULES += $(KERNEL_MODULES_OUT)/bolero_cdc_dlkm.ko \
--- qcom/opensource/audio-kernel/audio_kernel_product_board.mk
+++ qcom/opensource/audio-kernel/audio_kernel_product_board.mk
@@ -38,9 +38,10 @@ PRODUCT_PACKAGES += $(KERNEL_MODULES_OUT)/swr_dmic_dlkm.ko \
 endif
 ifeq ($(call is-board-platform-in-list,sun tuna), true)
 PRODUCT_PACKAGES += $(KERNEL_MODULES_OUT)/lpass_bt_swr_dlkm.ko
-PRODUCT_PACKAGES += $(KERNEL_MODULES_OUT)/wcd9378_slave_dlkm.ko \
-PRODUCT_PACKAGES += $(KERNEL_MODULES_OUT)/wcd9378_dlkm.ko \
-PRODUCT_PACKAGES += $(KERNEL_MODULES_OUT)/qmp_dlkm.ko
+	$(KERNEL_MODULES_OUT)/wcd9378_slave_dlkm.ko \
+	$(KERNEL_MODULES_OUT)/wcd9378_dlkm.ko \
+	$(KERNEL_MODULES_OUT)/qmp_dlkm.ko \
+	$(KERNEL_MODULES_OUT)/aw883xx_dlkm.ko
 endif
 ifeq ($(call is-board-platform-in-list,bengal holi blair), true)
 PRODUCT_PACKAGES += $(KERNEL_MODULES_OUT)/bolero_cdc_dlkm.ko \
